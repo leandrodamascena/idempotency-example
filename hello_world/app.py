ff98f427ed462fbffc76b2da689f4cf213d2d773
Leandro Damascena <leandro.damascena@gmail.com>
2022-12-30 16:21:40 +0000

    Adding files

---
+++ hello_world/app.py
@@ -0,0 +1,12 @@
+from aws_lambda_powertools.utilities.idempotency import (
+    DynamoDBPersistenceLayer, idempotent
+)
+
+persistence_layer = DynamoDBPersistenceLayer(table_name="idempotency-example-table")
+
+@idempotent(persistence_store=persistence_layer)
+def lambda_handler(event, context):
+    return {
+        "message": "success",
+        "statusCode": 200,
+    }
